d1f0213090fd05ef1a1010358a7f7aea7de61215
Syed Rizwan <syed.m.rizwan@outlook.com>
2023-07-06 00:17:59 +0500

    implemented binary search

---
+++ src/binary-search/binary_search.go
@@ -0,0 +1,16 @@
+package binary_search
+
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		m := (l + r) / 2
+		if nums[m] > target {
+			r = m - 1
+		} else if nums[m] < target {
+			l = m + 1
+		} else {
+			return m
+		}
+	}
+	return -1
+}
